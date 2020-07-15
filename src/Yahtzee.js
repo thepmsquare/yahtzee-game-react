81d96319b854675b519d7e2a9356aec41510aa4c
thePmSquare <impmmmm@gmail.com>
2020-07-15 09:52:59 +0530

    initial setup for yahtzee game

---
+++ src/Yahtzee.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./stylesheets/Yahtzee.css";
+
+class Yahtzee extends Component {
+  render() {
+    return (
+      <div className="Yahtzee">
+        <h1>Yahtzee</h1>
+      </div>
+    );
+  }
+}
+
+export default Yahtzee;
