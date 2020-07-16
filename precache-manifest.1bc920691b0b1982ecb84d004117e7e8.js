7e877eac525980e5da3a1b967d19eac7ba4f9568
thePmSquare <impmmmm@gmail.com>
2020-07-16 06:31:00 +0530

    Updates

---
+++ precache-manifest.1bc920691b0b1982ecb84d004117e7e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4c924b591464def57081d21bf9b89e",
+    "url": "/thepmsquare/yahtzee-game-react/index.html"
+  },
+  {
+    "revision": "22ebafbc45c118786e74",
+    "url": "/thepmsquare/yahtzee-game-react/static/css/main.59f4c3f5.chunk.css"
+  },
+  {
+    "revision": "6caf02e2f1a280956d2e",
+    "url": "/thepmsquare/yahtzee-game-react/static/js/2.b74b93d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thepmsquare/yahtzee-game-react/static/js/2.b74b93d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ebafbc45c118786e74",
+    "url": "/thepmsquare/yahtzee-game-react/static/js/main.6db48a89.chunk.js"
+  },
+  {
+    "revision": "0829eb6d968454929ca5",
+    "url": "/thepmsquare/yahtzee-game-react/static/js/runtime-main.0da2979e.js"
+  },
+  {
+    "revision": "450f757ba22bf3eb9f839eb26287398a",
+    "url": "/thepmsquare/yahtzee-game-react/static/media/SpaceMono-Bold.450f757b.ttf"
+  },
+  {
+    "revision": "233cc83beb6682c47f0d6a3ea129428f",
+    "url": "/thepmsquare/yahtzee-game-react/static/media/SpaceMono-Italic.233cc83b.ttf"
+  },
+  {
+    "revision": "52eb73dbce8bf4083a59acc2e91699ad",
+    "url": "/thepmsquare/yahtzee-game-react/static/media/SpaceMono-Regular.52eb73db.ttf"
+  }
+]);
